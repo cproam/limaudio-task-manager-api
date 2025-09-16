ddeec962b8ce8251432bb028b9df533150c5b18f
Almas <gdocs34@gmail.com>
2025-09-16 14:38:16 +0300

    feat: Register UserController in the router for handling user-related routes

--- public/index.php
+++ public/index.php
@@ -23,6 +23,7 @@ use App\Controllers\UploadController;
 use App\Controllers\TaskFeatureController;
 use App\Controllers\WebhookController;
 use App\Controllers\DirectionsController;
+use App\Controllers\UserController;
 use App\Database\DB;
 use App\Support\Env;
 use App\Security\Auth;
@@ -77,7 +78,7 @@ $router->scanControllers([
 	TaskFeatureController::class,
 	WebhookController::class,
 	DirectionsController::class,
+	UserController::class
 ]);
 
 $router->dispatch($request);
-
