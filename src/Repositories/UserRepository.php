5f8b4ee703a543eac3a83d9807c3fbae7543bedd
Almas <gdocs34@gmail.com>
2025-09-24 09:26:57 +0300

    fix: permissions update

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -163,7 +163,7 @@ class UserRepository
     {
         if (!$permissions) return;
         $this->pdo->prepare('DELETE FROM permissions WHERE user_id=?')->execute([$userId]);
-        $stmt = $this->pdo->prepare('INSERT OR IGNORE INTO permissions(name, user_id) VALUES(?, ?, ?)');
+        $stmt = $this->pdo->prepare('INSERT OR IGNORE INTO permissions(name, user_id) VALUES(?, ?)');
         foreach ($permissions as $pname) {
             $stmt->execute([$pname, $userId]);
         }
